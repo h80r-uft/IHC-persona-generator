d3cd1b15cddebd00bf745f86f6f7f6682cb71ed9
h80r <h80rgbc@gmail.com>
2021-10-12 11:40:27 -0300

    Create ImageService

---
+++ lib/services/image_service.dart
@@ -0,0 +1,18 @@
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
+
+class ImageService {
+  final _dio = Dio();
+
+  Future<Uint8List> getPerson() async {
+    final request = await _dio.get<Uint8List>(
+      'https://thispersondoesnotexist.com/image',
+      options: Options(responseType: ResponseType.bytes),
+    );
+
+    if (request.data == null) throw Exception("Can't load image.");
+
+    return request.data!;
+  }
+}
