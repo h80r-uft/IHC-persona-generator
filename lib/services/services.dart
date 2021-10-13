830a9ecad0d60a5e2b0e16a88d21c30696387938
h80r <h80rgbc@gmail.com>
2021-10-12 21:19:14 -0300

    Refactor service folder

---
+++ lib/services/services.dart
@@ -0,0 +1,10 @@
+// Packages
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Services
+import 'package:persona_generator/services/data_service/data_service.dart';
+import 'package:persona_generator/services/image_service/image_service.dart';
+
+final dataServiceProvider = Provider<DataService>((ref) => DataService());
+
+final imageServiceProvider = Provider<ImageService>((ref) => ImageService());
