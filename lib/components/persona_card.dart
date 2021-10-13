c4a387d3371305451b7eb0c4cb9435c19441ceb6
h80r <h80rgbc@gmail.com>
2021-10-13 13:02:48 -0300

    Modularize PersonaStats

--- lib/components/persona_card.dart
+++ lib/components/persona_card.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:persona_generator/components/persona_attribute.dart';
 import 'package:persona_generator/components/persona_description.dart';
 import 'package:persona_generator/components/persona_picture.dart';
+import 'package:persona_generator/components/persona_stats.dart';
 
 class PersonaCard extends StatelessWidget {
   const PersonaCard({
@@ -20,24 +21,26 @@ class PersonaCard extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: Row(
           mainAxisSize: MainAxisSize.min,
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: const [
-                Text('Press the button below!'),
+          children: const [
+            PersonaStats(
+              children: [
                 PersonaAttribute(
                   icon: Icons.family_restroom,
                   text: '3 filhos',
                   color: Colors.blue,
                 ),
+                PersonaAttribute(
+                  icon: Icons.calendar_today,
+                  text: '37 anos',
+                  color: Colors.red,
+                ),
               ],
             ),
-            const PersonaPicture(
+            PersonaPicture(
               image: 'https://www.w3schools.com/howto/img_avatar.png',
               color: Colors.blue,
             ),
-            const PersonaDescription(
+            PersonaDescription(
               name: 'Jack L Dardar',
               description:
                   'Sed accumsan ut elit suscipit eleifend. Suspendisse eget euismod arcu. Cras auctor sollicitudin tellus, id egestas dolor elementum sed. Donec imperdiet quis mauris eu laoreet. Sed urna velit, imperdiet sit amet erat non, molestie vehicula mauris. Ut eu auctor quam. In iaculis eros nec faucibus cursus. Duis feugiat posuere posuere. Ut elementum id felis hendrerit tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et bibendum enim. Cras varius ante sit amet arcu efficitur pharetra. Curabitur lobortis, lorem ac dapibus aliquam, eros sem tempus magna, hendrerit hendrerit nisi lectus in est.',
