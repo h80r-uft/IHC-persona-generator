264f3f52139e76c42b7919334e3b50782ac8b90b
h80r <h80rgbc@gmail.com>
2021-10-13 15:21:57 -0300

    Create footer credits

--- lib/pages/persona_page.dart
+++ lib/pages/persona_page.dart
@@ -18,6 +18,29 @@ class PersonaPage extends ConsumerWidget {
       body: const Center(
         child: PersonaCard(),
       ),
+      bottomSheet: Container(
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(20),
+          ),
+          color: Colors.black.withOpacity(0.3),
+        ),
+        child: Opacity(
+          opacity: 0.3,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text("This project wouldn't be possible without "),
+              Text('FakePersonGenerator'),
+              Text(" (used to generate a persona's data) and "),
+              Text('ThisPersonDoesNotExist'),
+              Text(" (persona's picture provider)"),
+            ],
+          ),
+        ),
+      ),
       floatingActionButton: CircularMenu(
         alignment: Alignment.bottomRight,
         items: [
