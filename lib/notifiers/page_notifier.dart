5aa7859220f5f2fa3579a335648380c430c46715
h80r <h80rgbc@gmail.com>
2021-10-13 23:36:30 -0300

    Add _generateDescription function

--- lib/notifiers/page_notifier.dart
+++ lib/notifiers/page_notifier.dart
@@ -1,4 +1,5 @@
 // Packages
+import 'dart:math';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Models
@@ -18,12 +19,40 @@ class PageNotifier extends StateNotifier<PageData> {
   final ImageService _imageService;
 
   void generatePersona() async {
-    state.copyWith(isLoading: true);
+    state = state.copyWith(isLoading: true);
     state = state.copyWith(
       personaData: await _dataService.getPersonaData(),
       personaImage: await _imageService.getPersonaImage(),
-      personaDescription: '',
+    );
+    state = state.copyWith(
+      personaDescription: _generateDescription(),
       isLoading: false,
     );
+  }
+
+  String _generateDescription() {
+    const templates = [
+      'Is a %ES% as a %JT% in the industry of %ID%. Likes %IT% and describes themselves as %BI%',
+      '%BI% A %ES% in the industry of %ID%. Is a %JT% interested in %IT%',
+      'Working in the field of %ID%, loves %IT%, currently is a %ES% as %JT%. %BI%'
+    ];
+
+    final data = state.personaData!;
+
+    final dataMap = {
+      '%ES%': data.employmentStatus,
+      '%JT%': data.jobTitle,
+      '%ID%': data.industry,
+      '%IT%': data.interest,
+      '%BI%': data.biography
+    };
+
+    var description = templates[Random().nextInt(templates.length)];
+
+    for (final key in dataMap.keys) {
+      description = description.replaceFirst(key, dataMap[key] ?? 'ERROR');
+    }
+
+    return description;
   }
 }
